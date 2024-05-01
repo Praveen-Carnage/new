3220dc438e9ecb8d97e3263432493e13620d897b
Praveen-Carnage <r.praveen.snp@gmail.com>
2024-05-01 19:36:07 +0530

    basic

---
+++ basic.java
@@ -0,0 +1,14 @@
+public class basic {
+
+  static int add(int a, int b) { return a + b; }
+
+  public static void main(String[] args) {
+
+    int num1 = 120;
+    int num2 = 7;
+    int result = add(num1, num2);
+
+    System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+
+  }
+}
